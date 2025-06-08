1d51a7fd9f5738ef72e50b1e21805ef20c5d7784
Daniil Mogilnyi <mogilnydaniel@gmail.com>
2025-06-08 19:36:17 +0300

    Initial Commit

---
+++ FinancialTracker/FinancialTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancialTrackerApp.swift
+//  FinancialTracker
+//
+//  Created by Daniel Mogilny on 6/8/25.
+//
+
+import SwiftUI
+
+@main
+struct FinancialTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
